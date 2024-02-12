ef680e59a18b6ac7c4c1439642c276e014abf950
sandeepNIS <sandeep.r@nichi.com>
2024-02-12 23:35:57 +0530

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,54 @@
+export const NAME = 'Sandeep R';
+
+export const DESIGNATION = `Software Engineer`;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'Bengaluru',
+  Age: '23',
+};
+
+export const SKILLS = [
+ 
+  {
+    title: 'React',
+    level: '75%',
+  },
+  {
+    title: 'Wordpress',
+    level: '75%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Java',
+    level: '50%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'Redux',
+  'HTML',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'REST API',
+  'MySQL',
+  'GitHub',
+];
+
+export const CONTACTS = {
+  EMAIL: 'sandeepkaniyar08@gmail.com',
+  PHONE: '+91 9739304928',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/osamajavaid',
+  LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
